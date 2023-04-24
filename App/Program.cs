a852ad974aa5fdf9ae13b1cfb31f9b90e5e0201d
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2023-04-24 19:26:15 +0500

    refactored

--- App/Program.cs
+++ App/Program.cs
@@ -1,32 +1,81 @@
 ﻿using Domain.Models;
 using Infrastructure.Services;
 
+Console.WriteLine("Welcome to the registration app v1");
 var employeeService = new EmployeeService();
+while (true)
+{
+    Console.WriteLine(@" Press
 
-var emp1 = new Employee("Firstaname1", "LastName1",123);
-var emp2 = new Employee("Firstaname2", "LastName2",123);
+    1 - Create new Employee 
+    2 - Update Employee
+    3 - Delete Employee
+    4 - Show List of employees
+    0 - Exit
+    ");
+    Console.Write("input-> ");
+    var input = Console.ReadLine();
+    
+    if (input == "0") break;
+    
+    bool result = input switch
+    {
+        "1" => Add(),
+        "2" => Update(),
+        "3" => Delete(),
+        "4" => Show(),
+        _ => false
+    };
 
-employeeService.AddEmployee(emp1);
-employeeService.AddEmployee(emp2);
+    if (result == false)
+        Console.WriteLine("Unknown Command");
+}
 
-Console.ReadLine();
-employeeService.Show();
+bool Add()
+{
+    var emp = new Employee();
+    Console.Write("Firstname: ");
+    emp.Firstname = Console.ReadLine();
+    Console.Write("Lastname: ");
+    emp.Lastname = Console.ReadLine();
+    Console.Write("Salary: ");
+    emp.Salary = Convert.ToDecimal(Console.ReadLine());
+        
+    employeeService.AddEmployee(emp);
+    return true;
+}
 
-emp1 = employeeService.GetEmployeeById(1);
-emp1.Firstname = "Abdulloh";
-emp1.Lastname = "Abdulloev";
-emp1.Salary = 5000m;
+bool Show()
+{
+    employeeService.Show();
+    return true;
+}
 
-employeeService.Update(emp1);
+bool Delete()
+{
+    Console.Write("Enter the Id of the Employee to delete: ");
+    int id = Convert.ToInt32(Console.ReadLine());
+    employeeService.DeleteEmployee(id);
+    return true;
+}
 
-Console.ReadLine();
-employeeService.Show();
+bool Update()
+{
+    Console.Write("Enter the Id of the Employee to update: ");
+    int id = Convert.ToInt32(Console.ReadLine());
+    
+    var employee = employeeService.GetEmployeeById(id);
+    Console.Write($"{employee.Firstname} --> ");
+    employee.Firstname = Console.ReadLine();
+    Console.Write($"{employee.Lastname} --> ");
+    employee.Lastname = Console.ReadLine();
+    Console.Write($"{employee.Salary} --> ");
+    employee.Salary = Convert.ToDecimal(Console.ReadLine());
+    
+    employeeService.Update(employee);
+    return true;
 
-employeeService.DeleteEmployee(1);
-
-Console.ReadLine();
-employeeService.Show();
-
+}
 
 
 
